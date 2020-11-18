15e3b825d1fcfc730657c2167ff436c86ab57d82
fako1024 <mail+github@f-ko.eu>
2020-11-18 16:32:39 +0100

    Add mock tests

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/fako1024/httpc v1.0.0
 	github.com/json-iterator/go v1.1.10
+	gopkg.in/h2non/gock.v1 v1.0.15
 )
---
+++ rc_test.go
@@ -0,0 +1,77 @@
+package rc
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"testing"
+
+	"gopkg.in/h2non/gock.v1"
+)
+
+var (
+	uri = "https://chat.example.org/hooks/UnpNPC3DWR0tMy"
+
+	testMessage = Request{
+		Channel: "@test",
+		User:    "user",
+		Emoji:   EmojiAlert,
+		Message: "Hello, world!",
+	}
+	invalidMessageNoChannel = Request{
+		Channel: "",
+		User:    "user",
+		Emoji:   EmojiAlert,
+		Message: "Hello, world!",
+	}
+	invalidMessageNoMessage = Request{
+		Channel: "@test",
+		User:    "user",
+		Emoji:   EmojiAlert,
+		Message: "",
+	}
+)
+
+func TestValidate(t *testing.T) {
+	if err := testMessage.Validate(); err != nil {
+		t.Fatalf("Failed to validate message: %s", err)
+	}
+	if err := invalidMessageNoChannel.Validate(); err == nil || err.Error() != "Channel parameter missing" {
+		t.Fatalf("Unexpected success when validating message without channel: %s", err)
+	}
+	if err := invalidMessageNoMessage.Validate(); err == nil || err.Error() != "Message parameter missing" {
+		t.Fatalf("Unexpected success when validating message without message text: %s", err)
+	}
+}
+
+func TestSendSimpleMessage(t *testing.T) {
+
+	// Set up a mock matcher
+	defer gock.Off()
+	gock.New(uri).
+		Post(path.Base(uri)).
+		MatchType("application/json").
+		AddMatcher(gock.MatchFunc(func(arg1 *http.Request, arg2 *gock.Request) (bool, error) {
+			data, err := ioutil.ReadAll(arg1.Body)
+			if err != nil {
+				return false, err
+			}
+
+			return bytes.Equal(data, []byte(`{"channel":"@test","username":"user","icon_emoji":":rotating_light:","text":"Hello, world!"}`)), nil
+		})).
+		Reply(http.StatusOK)
+
+	if err := Send(uri, testMessage); err != nil {
+		t.Fatalf("Failed to send message: %s", err)
+	}
+}
+
+func TestSendInvalidMessage(t *testing.T) {
+	if err := Send(uri, invalidMessageNoChannel); err == nil || err.Error() != "Error validating RocketChat request: Channel parameter missing" {
+		t.Fatalf("Unexpected success when sending message without channel: %s", err)
+	}
+	if err := Send(uri, invalidMessageNoMessage); err == nil || err.Error() != "Error validating RocketChat request: Message parameter missing" {
+		t.Fatalf("Unexpected success when sending message without message text: %s", err)
+	}
+}
